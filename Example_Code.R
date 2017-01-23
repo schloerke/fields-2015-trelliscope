60f0e2fb0eebff243060fa536adf2f1c864cb77c
Barret Schloerke <schloerke@gmail.com>
2017-01-23 13:08:26 -0500

    first commit

---
+++ Example_Code.R
@@ -0,0 +1,71 @@
+library(trelliscope)
+library(datadr)
+library(housingData)
+
+library(magrittr)
+library(dplyr)
+library(tidyr)
+
+# divide housing data by county and state
+divide(housing, by = c("county", "state")) %>%
+  drFilter(function(x){nrow(x) > 10}) ->
+  # drFilter(function(x){nrow(x) > 120}) ->
+  byCounty
+
+
+byCounty %>%
+  drLapply(function(x){
+    range(x[,c("medListPriceSqft", "medSoldPriceSqft")], na.rm = TRUE)
+  }) %>%
+  as.list() %>%
+  lapply("[[", 2) %>%
+  unlist() %>%
+  range() ->
+  yRanges
+
+
+priceCog <- function(x) {
+   zillowString <- gsub(" ", "-", do.call(paste, getSplitVars(x)))
+   list(
+      slope = cog(
+        coef(lm(medListPriceSqft ~ time, data = x))[2],
+        desc = "list price slope"
+      ),
+      meanList = cogMean(x$medListPriceSqft),
+      meanSold = cogMean(x$medSoldPriceSqft),
+      nObs = cog(
+        length(which(!is.na(x$medListPriceSqft))),
+        desc = "number of non-NA list prices"
+      ),
+      zillowHref = cogHref(
+        sprintf("http://www.zillow.com/homes/%s_rb/", zillowString),
+        desc = "zillow link"
+      )
+   )
+}
+
+
+
+latticePanel <- function(x) {
+  x %>%
+    select(time, medListPriceSqft, medSoldPriceSqft) %>%
+    gather(key = "variable", value = "value", medListPriceSqft, medSoldPriceSqft, -time) %>%
+    ggplot(aes(x = time, y = value, color = variable)) +
+      geom_smooth() +
+      geom_point() +
+      ylim(yRanges) +
+      labs(y = "Price / Sq. Ft.")
+}
+
+makeDisplay(
+  byCounty,
+  group   = "fields",
+  panelFn = latticePanel,
+  cogFn   = priceCog,
+  name    = "list_vs_time_ggplot",
+  desc    = "List and sold priceover time w/ggplot2",
+  conn    = vdbConn("vdb")
+)
+
+view()
+
